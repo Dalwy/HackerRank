bac4410d34883c553e8062c575e832ba8d44ec4c
Dalwy <dalwyxd12@gmail.com>
2020-09-05 17:15:36 -0500

    Forgot to post these oops

---
+++ Alternating Characters/AC.py
@@ -0,0 +1,11 @@
+def alternatingCharacters(s):
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        print(result)
